b113ecc03925e7e8360fefde9c1e32ff0806294c
TuanSrark <lecongtuan472004@gmail.com>
2025-11-23 16:03:59 +0700

    fix: implement tsx in package.json and fix docker-compose

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-import { hashPassword } from "../src/shared/utils/argon2.util";
-
+import * as argon2 from "argon2";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const roles = await prisma.role.createMany({
+  console.log("🌱 Start seeding...");
+
+  // 1. Create roles
+  await prisma.role.createMany({
     data: [
       { id: "b86dd951-4a46-4295-97f1-82015d02f672", name: "ADMIN" },
       { id: "cb8d828d-c0b9-460f-8b30-f7de4152e84f", name: "USER" }
@@ -13,28 +15,28 @@ async function main() {
     skipDuplicates: true
   });
 
-  // Hash mật khẩu admin
-  const hashed = await hashPassword("123456789");
+  // 2. Hash password for admin
+  const hashedPassword = await argon2.hash("123456789");
 
-  // Insert admin user
+  // 3. Create admin user
   await prisma.user.upsert({
     where: { email: "admin@booking.com" },
     update: {},
     create: {
       email: "admin@booking.com",
-      password: hashed,
+      password: hashedPassword,
       roleId: "b86dd951-4a46-4295-97f1-82015d02f672"
     }
   });
+
+  console.log("✅ Seeding completed.");
 }
 
 main()
-  .then(async () => {
-    console.log('🌱 Seeding completed.');
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
+  .catch((e) => {
+    console.error("❌ Seeding error:", e);
     process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
   });
